7a63eef9249b446d0d7fb18c4bf9c8ea80303c12
Lucas Churchill <lucas@agtlucas.com>
2014-03-02 00:54:29 -0300

    Update config.rb

---
+++ blood-brothers/config.rb
@@ -0,0 +1,16 @@
+preferred_syntax = :sass
+
+http_path = ''
+
+css_dir = '../assets/css'
+sass_dir = '../assets/sass'
+# images_dir = 'src/images'
+javascripts_dir = '../assets/js'
+
+relative_assets = false
+line_comments = true
+output_style = :compressed
+
+asset_cache_buster :none
+
+Sass::Script::Number.precision = 8
