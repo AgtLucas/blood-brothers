a275a75812202c8c273943d33ecb2edb11ac66dc
Lucas Churchill <lucas@agtlucas.com>
2014-03-02 00:49:05 -0300

    Update Gruntfile.js

--- blood-brothers/Gruntfile.js
+++ blood-brothers/Gruntfile.js
@@ -5,6 +5,8 @@ module.exports = function (grunt) {
 
     // Show elapsed time after tasks run
     require('time-grunt')(grunt);
+    // Load all Grunt tasks
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
         jshint: {
